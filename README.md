312464092b22727d3f11c323e2d0c707fd9572f3
Vito <vito@dwave.cc>
2023-03-11 16:12:06 +0800

    feat: add main features

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LineStickerToolkit
+
 A command-line tool for converting Line stickers into various image formats.
+
+##
+
+```shell
+pip install -r requirements.txt
+```
+
+## project tree
+
+├── modules/
+│ ├── **init**.py
+│ ├── module1.py
+│ └── module2.py
+├── main.py
+├── README.md
+└── requirements.txt
--- merge.py
+++ merge.py
@@ -1,19 +1,28 @@
 from typing import List
 
+
+path_input = "assets/inputs/command"
+path_output = "assets/outputs"
+
+
 body_list: List[str] = []
-prefix: str = ''
-suffix: str = ''
+prefix: str = ""
+suffix: str = ""
 
-with open('body.txt', 'r') as file:
+
+with open(f"{path_input}/body.txt", "r") as file:
     body_list = file.readlines()
-    body_list = [s.replace('\n', '') for s in body_list]
+    body_list = [s.replace("\n", "") for s in body_list]
 
-with open('prefix.txt', 'r') as file:
+
+with open(f"{path_input}/prefix.txt", "r") as file:
     prefix = file.read()
 
-with open('suffix.txt', 'r') as file:
+
+with open(f"{path_input}/suffix.txt", "r") as file:
     suffix = file.read()
 
-with open('full-command.txt', 'w') as file:
+
+with open(f"{path_output}/full-command.txt", "w") as file:
     for body in body_list:
         file.write(f"{prefix} {body}{suffix}\n")
